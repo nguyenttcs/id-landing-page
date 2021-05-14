9bc6d4af5e9fea35a8167b3fb758009b97aeb7b9
Iris Nguyen <nguyentt.ir@gmail.com>
2021-05-14 12:11:50 +0700

    init project

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+// $(document).ready(function(){
+//   $(".js-icon-contact").click(function(){
+// 		$("#popup-address").toggleClass("display");
+// 	});
+
+// 	$(".js-icon-close-address").click(function(){
+// 		$("#popup-address").toggleClass("display");
+// 	});
+
+// 	$(".js-icon-hambuger").click(function(){
+// 		$("#popup-menu").toggleClass("display");
+// 	});
+
+// 	$(".js-icon-close-menu").click(function(){
+// 		$("#popup-menu").toggleClass("display");
+// 	});
+
+// 	$(".js-menu-redirect").click(function(){
+// 		$("#popup-menu").toggleClass("display");
+// 	});
+		
+// });
